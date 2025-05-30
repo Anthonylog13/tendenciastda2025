351b07577bf85ed379ebd72fe0b9e77e9070bb06
Anthony Arango <anthonyarango01@gmail.com>
2025-05-29 23:54:21 -0500

    prueba3

--- gestionPedidos/gestionPedidos/wsgi.py
+++ gestionPedidos/gestionPedidos/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestionPedidos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestionPedidos.gestionPedidos.settings')
 
 application = get_wsgi_application()
 
